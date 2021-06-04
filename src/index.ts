d8877b19aebabc0f9d5804cabd2285c9d341c3b1
hkai-tony-li <tony.li@hkai.io>
2021-06-05 01:05:04 +0800

    Use passport basic auth

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import passport from 'passport';
+import session from 'express-session';
+import { BasicStrategy } from 'passport-http';
+
 import config from '../lib/config';
 
-
-// const PORT : number = parseInt(process.env["PORT"], 10) || 8080;
 const PORT = config.PORT;
 
 const app = express();
@@ -12,7 +14,52 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+app.use(express.static("public"));
+app.use(session({ secret: "cats" }));
+
+passport.use(new BasicStrategy(
+  (username: string, password: string, done: Function) => {
+    console.log({ username, password, done})
+    return done(null, { username, password })
+    return done(null, false, { message: 'Invalid username or password.' });
+  }
+))
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req: Request, res: Response, next) => {
+  res.status(200).send('Hello world');
+});
+
+app.get('/unauthorized', (req: Request, res: Response, next) => {
+  res.status(401).send();
+});
+
+app.get('/token', 
+  passport.authenticate('basic', { session: false }),
+  (req: Request, res: Response, next) => {
+  console.log(req.user)
+  res.status(200).send({
+    access_token: 'test',
+    refresh_token: 'test',
+    expires_in: 123,
+    token_type: 'bearer'
+  });
+});
+
+app.post('/login',
+  passport.authenticate('basic', { successRedirect: '/token' }));
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
-})
+});
+ 
