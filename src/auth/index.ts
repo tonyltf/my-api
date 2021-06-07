4309cb28e04b4f06ffe0e2dcde11132e1e589b57
hkai-tony-li <tony.li@hkai.io>
2021-06-07 22:15:13 +0800

    Prettier

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -16,25 +16,22 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
+app.use(express.static('public'));
+app.use(session({ secret: 'cats' }));
 
-passport.use(new BasicStrategy(
-  async (username: string, password: string, done: Function) => {
+passport.use(
+  new BasicStrategy(async (username: string, password: string, done: Function) => {
     try {
-
       const result: any = await getUser({ username, password });
-      console.log({ result })
       if (result) {
-        return done(null, { uid: result.uid })
+        return done(null, { uid: result.uid });
       }
     } catch (e) {
       console.error(e);
     }
-    return done(null, false);
-    // return done(null, false, { message: 'Invalid username or password.' });
-  }
-))
+    return done(null, false, { message: 'Invalid username or password.' });
+  })
+);
 
 passport.serializeUser((user: any, done) => {
   done(null, user);
@@ -45,39 +42,30 @@ passport.deserializeUser((user: any, done) => done(null, user));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.post('/login',
-  passport.authenticate('basic', { successRedirect: '/token' }));
+app.post('/login', passport.authenticate('basic', { successRedirect: '/token' }));
 
-app.get('/token', 
-  passport.authenticate('basic', { session: false }),
-  (req: Request, res: Response, next) => {
+app.get('/token', passport.authenticate('basic', { session: false }), (req: Request, res: Response, next) => {
   const payload = req.user;
-  console.log({ payload });
   const { generateToken } = services;
-
   res.status(200).send(generateToken(payload));
 });
 
 app.post('/register', async (req: Request, res: Response, next) => {
   try {
-
     const { username, password } = req.body;
     const { createAccount } = services;
-
     if (username && password) {
       const result = await createAccount({ username, password });
       if (result) {
-        res.status(200).send({ success: true })
-      } 
+        res.status(200).send({ success: true });
+      }
     }
   } catch (e) {
     res.status(500).send();
   }
   res.status(400).send({ success: false });
-
 });
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
- 
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
-import passport from 'passport';
-import session from 'express-session';
-import { BasicStrategy } from 'passport-http';
 
 import config from './lib/config';
 
@@ -14,52 +11,10 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
-app.use(session({ secret: "cats" }));
-
-passport.use(new BasicStrategy(
-  (username: string, password: string, done: Function) => {
-    console.log({ username, password, done})
-    return done(null, { username, password })
-    return done(null, false, { message: 'Invalid username or password.' });
-  }
-));
-
-passport.serializeUser((user: any, done) => {
-  done(null, user);
-});
-
-passport.deserializeUser((user: any, done) => {
-  done(null, user);
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
-
+app.use(express.static('public'));
 app.get('/', (req: Request, res: Response, next) => {
   res.status(200).send('Hello world');
 });
-
-app.get('/unauthorized', (req: Request, res: Response, next) => {
-  res.status(401).send();
-});
-
-app.get('/token', 
-  passport.authenticate('basic', { session: false }),
-  (req: Request, res: Response, next) => {
-  console.log(req.user)
-  res.status(200).send({
-    access_token: 'test',
-    refresh_token: 'test',
-    expires_in: 123,
-    token_type: 'bearer'
-  });
-});
-
-app.post('/login',
-  passport.authenticate('basic', { successRedirect: '/token' }));
-
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
 });
- 
